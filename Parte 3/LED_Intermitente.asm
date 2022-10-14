f9d67390c01325f96791de9e881a44f446e48fb9
apochiega <104432368+apochiega@users.noreply.github.com>
2022-10-14 11:11:46 -0300

    Update LED_Intermitente.asm

--- Parte 3/LED_Intermitente.asm
+++ Parte 3/LED_Intermitente.asm
@@ -18,63 +18,61 @@
 .def OutRegister = r20
 .def Contador1 = r23
 .def Contador2 = r22
-.def Contador3 = r21
+.def ContadorIn = r24
 
 ldi OutRegister, LED1
 out DDRB, OutRegister
 
 start:
 	ldi OutRegister, (LED1^LED1)
-    out PORTB, OutRegister
-    rcall delay_500ms
+	out PORTB, OutRegister
+	
+	ldi ContadorIn, 1
+	rcall delay_500ms
     
 	ldi OutRegister, LED1
-    out PORTB, OutRegister
-    rcall delay_500ms
+	out PORTB, OutRegister
+	
+	ldi ContadorIn, 1
+	rcall delay_500ms
     
     rjmp start
     
 
 // ***************************************
-// delay_500ms
-// Esta función hace un delay de 500ms.
-// Sin argumento de entrada.
+// delay_ms
+// Esta función hace un delay de 500ms, repitiéndose una cantidad de veces dada por el ingreso.
+// Argumento de entrada en ContadorIn (r24). Valores Válidos (1:255)
 // ***************************************
-delay_500ms:
+delay_ms:
 	push Contador1
 	push Contador2
-	push Contador3
 	
 	ldi Contador1, 255	// 1 clk
-	ldi Contador2, 255	// 1 clk
-	ldi Contador3, 41	// 1 clk
+	ldi Contador2, 197	// 1 clk
 	// Estos 3 clks se agregan al final de la cuenta, porque no estan loopeados
 
 	loop1:
-	dec Contador1		// 1 clk
-	cpi Contador1, 0	// 1 clk
+	dec Contador1		// 1 clk - Settea el flag Z si es 0
 	
 	brne loop1	// 1/2 clk
 	// Se hace 255 veces el loop de 3 clks
 
-		dec Contador2		// 1 clk
 		ldi Contador1, 255	// 1 clk
+		dec Contador2		// 1 clk - Settea el flag Z si es 0
 
-		cpi Contador2, 0	// 1clk
-		brne loop1	// 1/2 clk
-		// Se hace 255 veces el loop de 4 clks y repite 255 veces el ciclo anterior
-		// 196.095 clks
+		brne loop1	// 2 clk (-1 al final)
+		// Se hace 21 veces el loop de 4 clks y repite 21 veces el ciclo anterior
+		// 16.150 clks = (aprox) 1,009ms
 
-			dec Contador3		// 1 clk
 			ldi Contador1, 255	// 1 clk
-			ldi Contador2, 255	// 1 clk
+			ldi Contador2, 197	// 1 clk
+			dec ContadorIn		// 1 clk - Settea el flag Z si es 0
 
-			cpi Contador3, 0	// 1 clk
-			brne loop1	// 1/2 clk
+			brne loop1	// 2 clk (-1 al final)
 			// Se hace 41 veces el loop de 5 clks y repite 41 veces el ciclo anterior de 196.095 clks
 			//El ciclo demora 8.040.100 clks = 0,5025 s
-	
-	pop Contador3
+
 	pop Contador2
 	pop Contador1
 	ret 
